180e1d425e3b7caab7cda8a76908cbdf72a26436
Julie J <jeannet.julie@gmail.com>
2021-05-11 23:27:15 +0200

    utilisation méthode récupérée pour Décoder l'image codée en base 64 afin d'obtenir un Blob

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 import { WebcamImage, WebcamInitError,WebcamUtil } from 'ngx-webcam';
-import { Observable, Subject } from 'rxjs';
+import { Observable, Subject, Observer  } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -22,6 +22,8 @@ export class AppComponent {
   // qui sera ultérieurement conservée sur le serveur
   public webcamImage: WebcamImage = null;
 
+  generatedImage: string;
+
   // webcam snapshot trigger pour déclencher la prise de photo
   // on utilise Subject qui dans le monde de rxjs est un observable et un observeur
   private trigger: Subject<void> = new Subject<void>();
@@ -40,10 +42,43 @@ export class AppComponent {
   public handleImage(webcamImage: WebcamImage): void{
     console.log('received webcam image', webcamImage);
     this.webcamImage = webcamImage;
+
+    this.dataURItoBlob(webcamImage.imageAsBase64).subscribe(
+      (blob) => {
+        const imageBlob: Blob = blob;
+        const imageName: string = new Date().toISOString();
+        const imageFile: File = new File([imageBlob], imageName, {
+          type: 'image/jpeg',
+        });
+        console.log('imageFile', imageFile);
+
+        this.generatedImage = window.URL.createObjectURL(imageFile);
+        console.log('generatedImage', this.generatedImage);
+        window.open(this.generatedImage);
+      },
+      (err) => console.error(err),
+      () => console.log('completed')
+    );
+
   }
 
   public get triggerObservable(): Observable<void>{
     return this.trigger.asObservable();
+  }
+  // Method to convert Base64Data Url as Image Blob
+  // https://medium.com/better-programming/convert-a-base64-url-to-image-file-in-angular-4-5796a19fdc21
+  dataURItoBlob(dataURI: string): Observable<Blob> {
+    return Observable.create((observer: Observer<Blob>) => {
+      const byteString: string = window.atob(dataURI);
+      const arrayBuffer: ArrayBuffer = new ArrayBuffer(byteString.length);
+      const int8Array: Uint8Array = new Uint8Array(arrayBuffer);
+      for (let i = 0; i < byteString.length; i++) {
+        int8Array[i] = byteString.charCodeAt(i);
+      }
+      const blob = new Blob([int8Array], { type: 'image/jpeg' });
+      observer.next(blob);
+      observer.complete();
+    });
   }
 
 }
