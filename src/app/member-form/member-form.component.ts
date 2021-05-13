27e7ec4f93a4eae775eef7879f9c62086e065ada
Julie J <jeannet.julie@gmail.com>
2021-05-14 00:12:04 +0200

    creation d'un formulaire de nouveau membre

--- src/app/member-form/member-form.component.ts
+++ src/app/member-form/member-form.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { IDatePickerConfig } from 'ng2-date-picker';
 
 @Component({
   selector: 'app-member-form',
   templateUrl: './member-form.component.html',
-  styleUrls: ['./member-form.component.css']
+  styleUrls: ['./member-form.component.css'],
 })
 export class MemberFormComponent implements OnInit {
+  createMember: FormGroup;
+  datePickerConfig: IDatePickerConfig = { locale: 'fr', showMultipleYearsNavigation: true };
+  @Input() imageName;
 
-  constructor() { }
+  // comme on utilise ReactiveFormsModule,
+  // le formulaire est déjà crée côté ts
+  // puis on lie FormGroup au formulaire côté html
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.createMember = this.fb.group({
+      name: ['', Validators.required],
+      firstName: ['', Validators.required],
+      dob: [''],
+      imageName: this.imageName
+    });
   }
 
+  create() {
+    console.log(this.createMember.value);
+  }
 }
